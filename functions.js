bd07d7519b40d02b40bd6614ff71c6378dece7f5
clarissamk48 <47182540+clarissamk48@users.noreply.github.com>
2021-05-04 15:20:50 -0400

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,79 @@
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 1000, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  }); 
+var firstName = "";
+		var lastName = "";
+		var email = "";
+		var dType = "";
+		var receipt = "";
+		var anon = "";
+		var list = "";
+		var amount = "";
+		
+		$('.set-amount').autoGrow(0);
+		
+		/*
+			if(isiPad || jQuery.browser.mobile){
+				$('#team').hide()
+				$('.team-mobile').show();	
+			}else{
+				$('#team').show()
+				$('.team-mobile').hide();
+			}
+		*/
+		
+		//Set & Highlight Donation Amount
+		$(".button").click(function(){
+			$(".button").removeClass("selected");
+			$(this).addClass("selected");
+			
+			$(this).find("input").focus();
+		});
+		
+		//Grow the donation box if they type more than 4 numbers
+		$(".set-amount").keyup(function(){
+			
+			if (this.value != this.value.replace(/[^0-9\.]/g, '')) {
+		       this.value = this.value.replace(/[^0-9\.]/g, '');
+		    }
+
+		});
+		
+		
+		$("input").on("change", function(){
+			// $(".donation-box").css("height", "500px");
+			
+			firstName = $("#firstName").val();
+			lastName = $("#lastName").val();
+			email = $("#email").val();
+			
+			if ( $("#one-time").prop( "checked" ) ){
+				dType = "One-Time";
+			}
+			if ( $("#monthly").prop( "checked" ) ){
+				dType = "Monthly";
+			}
+
+		});
+    
+});
